d6e5bcc88bd480a5be9c4aeca67655f1748242a1
thain <thain@DESKTOP-R610K2J>
2020-05-18 10:01:15 -0300

    Melhorias no Cenario

--- src/test/java/simulador/investimento/API/TesteSimuladorAPICenarios.java
+++ src/test/java/simulador/investimento/API/TesteSimuladorAPICenarios.java
@@ -1,28 +1,63 @@
 package simulador.investimento.API;
 
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.notNullValue;
+
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
-import io.restassured.RestAssured;
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
 
-
-public class TesteSimuladorAPICenarios extends TesteSimuladorInvestimentoAPI {
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class TesteSimuladorAPICenarios  {
 	
+String uriBase = "http://5b847b30db24a100142dce1b.mockapi.io/api/v1/simulador";
+           
 	
-
-
-	@Test 
-	public void naoDeveAcessarAPISemToken () {
-		RestAssured.given()
+	@Test
+	public void testConsultarSimulacaoGet() {
+		
+		given()
+			.relaxedHTTPSValidation()
+			.param("id", "1")
 		.when()
+			.get(uriBase)
 		.then()
-		;
-		
-		
-		
+			.body("id", notNullValue())
+			.body("meses", notNullValue())
+			.body("valor", notNullValue())
+			.statusCode(200)
+			.contentType(ContentType.JSON);
 	}
 	
-	
-	
-	
+	@Test
+	public void testCriarSimulacaoPost() {
+		
+		given()
+			.relaxedHTTPSValidation()
+			.body("{\"meses\" : \"20\"}")
+			.body("{\"valor\" : \"100.00\"}")
+		.when()
+			.post(uriBase)
+		.then()
+			.statusCode(400)
+			.contentType(ContentType.JSON);
+	}
 
+		
+	@Test
+	public void testSucessoApiSemAutenticacao() {
+		RestAssured.authentication = RestAssured.DEFAULT_AUTH;
+		
+		given()
+		.relaxedHTTPSValidation()
+	.when()
+		.get(uriBase)
+	.then()
+		.statusCode(200);
+	}	
+	
 }
